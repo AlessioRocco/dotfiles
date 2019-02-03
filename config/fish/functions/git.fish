7834e33b29fe0daa1e6f9bead526ea41ac755cd9
Alessio Rocco <alessio.rocco.lt@gmail.com>
2019-02-03 22:20:47 +0100

    [FISH] Add (git)hub alias
    https://github.com/github/hub#aliasing

---
+++ config/fish/functions/git.fish
@@ -0,0 +1,5 @@
+# Alias for hub, which wraps git to provide extra functionality with GitHub.
+
+function git --wraps hub --description 'Alias for hub, which wraps git to provide extra functionality with GitHub.'
+  hub $argv
+end

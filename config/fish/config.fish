aafe1153a56f24e3145e6536cca193adb501e4f8
Alessio Rocco <alessio.rocco.lt@gmail.com>
2019-02-03 23:24:20 +0100

    [SETUP] Install asdf
    https://github.com/asdf-vm/asdf

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,3 +5,8 @@ set -xg CDPATH . ~/code
 
 # Set NeoVim as the default editor.
 set -xg EDITOR "nvim"
+
+### Configurations
+
+# Asdf - leave this at the bottom of the file
+source /usr/local/opt/asdf/asdf.fish

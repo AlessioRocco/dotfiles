c2be18abc211ab8c0e89dc0b1b24609668652901
Alessio Rocco <alessio.rocco.lt@gmail.com>
2019-03-26 21:24:29 +0100

    [FISH] Add ~/.bin to $PATH

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,6 +6,9 @@ set -xg CDPATH . ~/code
 # Set NeoVim as the default editor.
 set -xg EDITOR "nvim"
 
+# Set $PATH
+set PATH ~/.bin $PATH
+
 ### Configurations
 
 # Asdf - leave this at the bottom of the file.

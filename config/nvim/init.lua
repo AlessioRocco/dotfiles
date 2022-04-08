504c87623248027be08af22beee2ed0861a95abe
Alessio Rocco <alessiorocco@nebulab.com>
2022-04-08 17:36:02 +0200

    [NVIM] Move latest config to Lua

---
+++ config/nvim/init.lua
@@ -0,0 +1,3 @@
+require('keybindings')
+require('plugins')
+require('settings')
--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -30,3 +30,13 @@ vim.opt.undofile      = true                          -- Automatically save your
 -- Theme preferences
 vim.opt.termguicolors = true                          -- Set up true color support
 vim.cmd[[colorscheme dracula]]                        -- Set Dracula as default theme
+
+-- Automatically save changes to disk without having to use :w
+vim.cmd([[
+  augroup update_buffers
+    au!
+    au FocusGained * :checktime     " Auto read buffers when focus is gained
+    au FocusLost * wall             " Auto save buffers when focus is lost
+    au BufLeave * wall              " Write all changed buffers when leaving a buffer
+  augroup END
+]])

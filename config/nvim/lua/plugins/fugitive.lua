0a45c93e957928beb16697136b5acc70076c6e90
Alessio Rocco <alessiorocco@nebulab.com>
2022-04-14 00:07:04 +0200

    [NVIM] Update fugitive keybindings

--- config/nvim/lua/plugins/fugitive.lua
+++ config/nvim/lua/plugins/fugitive.lua
@@ -1,7 +1,7 @@
 local utils = require("utils")
 local nnoremap = utils.nnoremap
 
-nnoremap('<leader>gs', ':Git<cr>')
-nnoremap('<leader>gd', ':Gvdiff<cr>')
-nnoremap('<leader>gc', ':Gcommit<cr>')
-nnoremap('<leader>gb', ':Gbrowse<cr>')
+nnoremap('<leader>b', '<cmd>GBrowse<cr>')
+nnoremap('<leader>c', '<cmd>Git commit<cr>')
+nnoremap('<leader>d', '<cmd>Gvdiff<cr>')
+nnoremap('<leader>s', '<cmd>Git<cr>')

83fc818e845e8e3695c67f8390a7dcc395695034
Alessio Rocco <alessiorocco@nebulab.com>
2022-11-04 15:55:48 +0100

    [NVIM] Update LspConfig keymaps

--- config/nvim/lua/plugins/lsp_config.lua
+++ config/nvim/lua/plugins/lsp_config.lua
@@ -1,18 +1,14 @@
-local keymap = vim.api.nvim_set_keymap
-local opts = { noremap = true }
+local utils = require("utils")
+local nnoremap = utils.nnoremap
 
-local function nkeymap(key, map)
-  keymap('n', key, map, opts)
-end
-
-nkeymap('gd', ':lua vim.lsp.buf.definition()<cr>')
-nkeymap('gD', ':lua vim.lsp.buf.declaration()<cr>')
-nkeymap('gi', ':lua vim.lsp.buf.implementation()<cr>')
-nkeymap('gw', ':lua vim.lsp.buf.document_symbol()<cr>')
-nkeymap('gw', ':lua vim.lsp.buf.workspace_symbol()<cr>')
-nkeymap('gr', ':lua vim.lsp.buf.references()<cr>')
-nkeymap('gt', ':lua vim.lsp.buf.type_definition()<cr>')
-nkeymap('K', ':lua vim.lsp.buf.hover()<cr>')
-nkeymap('<c-k>', ':lua vim.lsp.buf.signature_help()<cr>')
-nkeymap('<leader>af', ':lua vim.lsp.buf.code_action()<cr>')
-nkeymap('<leader>rn', ':lua vim.lsp.buf.rename()<cr>')
+nnoremap('gd', ':lua vim.lsp.buf.definition()<cr>')
+nnoremap('gD', ':lua vim.lsp.buf.declaration()<cr>')
+nnoremap('gi', ':lua vim.lsp.buf.implementation()<cr>')
+nnoremap('gw', ':lua vim.lsp.buf.document_symbol()<cr>')
+nnoremap('gW', ':lua vim.lsp.buf.workspace_symbol()<cr>')
+nnoremap('gR', ':lua vim.lsp.buf.references()<cr>')
+nnoremap('gt', ':lua vim.lsp.buf.type_definition()<cr>')
+nnoremap('K', ':lua vim.lsp.buf.hover()<cr>')
+nnoremap('<c-k>', ':lua vim.lsp.buf.signature_help()<cr>')
+nnoremap('<leader>af', ':lua vim.lsp.buf.code_action()<cr>')
+nnoremap('<leader>rn', ':lua vim.lsp.buf.rename()<cr>')

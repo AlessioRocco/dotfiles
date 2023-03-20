8b3cc4b37759b3c3b0136eaf59b19c008b5e3fff
Alessio Rocco <alessiorocco@nebulab.com>
2023-03-20 22:04:26 +0100

    [NVIM] Add vim-rails plugin
    https://github.com/tpope/vim-rails

---
+++ config/nvim/lua/plugins/rails.lua
@@ -0,0 +1,13 @@
+return {
+  -- Add Rails.vim
+  -- https://github.com/tpope/vim-rails
+  {
+    "tpope/vim-rails",
+    keys = {
+      { "<leader>ra", "<cmd>A<cr>", desc = "Rails: Alternate" },
+      { "<leader>rA", "<cmd>AV<cr>", desc = "Rails: Alternate split" },
+      { "<leader>rr", "<cmd>R<cr>", desc = "Rails: Relative" },
+      { "<leader>rR", "<cmd>RV<cr>", desc = "Rails: Relative split" },
+    },
+  },
+}

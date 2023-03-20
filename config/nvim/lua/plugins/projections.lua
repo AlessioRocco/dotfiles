0b58138fbf07ada52513367b16dcef5375415d75
Alessio Rocco <alessiorocco@nebulab.com>
2023-03-20 22:47:22 +0100

    [NVIM] Add projectionist.vim
    https://github.com/tpope/vim-projectionist

---
+++ config/nvim/lua/plugins/projections.lua
@@ -0,0 +1,26 @@
+return {
+  -- projectionist.vim: Granular project configuration
+  -- https://github.com/tpope/vim-projectionist
+  {
+    "tpope/vim-projectionist",
+    ft = { "ruby", "eruby" },
+    config = function()
+      vim.cmd([[
+      let g:rails_projections = {
+      \   "app/components/*_component.rb": {
+      \     "alternate": "app/components/{}_component/{basename}_component.html.erb"
+      \   },
+      \   "app/components/*_component.html.erb": {
+      \     "alternate": "app/components/{dirname}/../{basename}_component.rb"
+      \   },
+      \   "app/extensions/*_extension.rb": {
+      \     "alternate": "spec/{dirname}/{basename}_spec.rb"
+      \   },
+      \   "spec/*_spec.rb": {
+      \     "alternate": "app/extensions/{dirname}/{basename}_extension.rb"
+      \   },
+      \ }
+      ]])
+    end,
+  },
+}

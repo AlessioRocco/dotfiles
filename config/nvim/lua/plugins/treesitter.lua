d18b7a3d8d6541d4607f91d5e9dcae3b7529dbe1
Alessio Rocco <alessiorocco@nebulab.com>
2023-03-24 20:49:18 +0100

    [NVIM] Ensure all tree-sitter parser are installed

---
+++ config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,10 @@
+return {
+  -- Nvim Treesitter configurations and abstraction layer
+  -- https://github.com/nvim-treesitter/nvim-treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = "all",
+    },
+  },
+}

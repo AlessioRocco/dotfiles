0875c91bba7361cd0fe49b49166e0cf41b0638fb
Alessio Rocco <alessiorocco@nebulab.com>
2023-03-24 20:46:32 +0100

    [NVIM] Set basic custom options

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Autoread and autowrite
+vim.opt.autowriteall = true -- Like 'autowrite' but also used for commands
+vim.opt.swapfile = false -- Don't create a swapfile for a new buffer
+vim.opt.writebackup = false -- Don't backup before overwriting a file

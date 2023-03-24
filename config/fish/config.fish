a9883ec78104414b2355da8d8f5b8f32353ff376
Alessio Rocco <alessiorocco@nebulab.com>
2023-03-24 21:04:48 +0100

    [FISH] Lint fish config file

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,18 +4,18 @@
 set -xg CDPATH . ~/code
 
 # Set NeoVim as the default editor.
-set -xg EDITOR "nvim"
+set -xg EDITOR nvim
 
 # Editor for opening gems
-set -xg BUNDLER_EDITOR "nvim"
+set -xg BUNDLER_EDITOR nvim
 
 # Use ripgrep as default command for fzf
-set -gx FZF_DEFAULT_COMMAND  'rg --files --hidden'
+set -gx FZF_DEFAULT_COMMAND 'rg --files --hidden'
 
 # Force GitHub CLI to use colors
 set -xg CLICOLOR_FORCE true
 set -xg GH_FORCE_TTY true
-set -xg GLAMOUR_STYLE 'dark'
+set -xg GLAMOUR_STYLE dark
 
 ### Configurations
 
@@ -41,35 +41,35 @@ fish_add_path --move .git/safe/../../bin
 ### Aliases
 
 # Git/GitHub CLI
-alias g "git_or_gitsh"
-alias pr "fzf_pull_requests"
+alias g git_or_gitsh
+alias pr fzf_pull_requests
 
 # Reload fish shell configuration
-alias reload "reload_configuration"
+alias reload reload_configuration
 
 # Editor
-alias e "editor"
+alias e editor
 
 # Edit host database file
 alias eh "sudo vim /etc/hosts"
 
 # Restart postgresql
-alias pgr "restart_postgresql"
+alias pgr restart_postgresql
 
 # Kill process
-alias kp "kill_process"
+alias kp kill_process
 
 ### Abbreviations
 
 # GitHub CLI
-abbr h "gh"
+abbr h gh
 
 # Rails
-abbr r "rails"
-abbr k "rake"
+abbr r rails
+abbr k rake
 
 # Bundler
-abbr b "bundle"
+abbr b bundle
 abbr be "bundle exec"
 abbr bi "bundle install"
 abbr bu "bundle update"

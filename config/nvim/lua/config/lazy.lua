daa9ac1fcd8e9777171a0bbf77c1bb5e5bbcf42b
Alessio Rocco <alessiorocco@nebulab.com>
2023-09-13 17:33:25 +0200

    [NVIM] Add lang.yaml extra plugin to LazyVim
    https://www.lazyvim.org/extras/lang/yaml

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.ruby" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
     -- import/override with your plugins
     { import = "plugins" },
   },

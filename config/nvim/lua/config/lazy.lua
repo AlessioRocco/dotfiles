ef3963fd10de6dfa1adacea17a8525d5e9c71bae
Alessio Rocco <alessiorocco@nebulab.com>
2023-09-13 17:28:19 +0200

    [NVIM] Add Ruby extra plugin to LazyVim
    https://www.lazyvim.org/extras/lang/ruby

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.ruby" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- config/nvim/lua/plugins/neotest.lua
+++ config/nvim/lua/plugins/neotest.lua
@@ -1,14 +1,5 @@
 return {
   "nvim-neotest/neotest",
-  dependencies = {
-    "olimorris/neotest-rspec",
-  },
-  opts = {
-    -- Can be a list of adapters like what neotest expects,
-    -- or a table of adapter names, mapped to adapter configs.
-    -- The adapter will then be automatically loaded with the config.
-    adapters = { ["neotest-rspec"] = {} },
-  },
   keys = {
     {
       "<leader>ta",

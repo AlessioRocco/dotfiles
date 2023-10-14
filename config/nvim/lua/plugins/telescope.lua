5a55c6d3d9f154b6ac1060fc5f2cb3af878446a6
Alessio Rocco <alessiorocco@nebulab.com>
2023-10-14 12:17:02 +0200

    [NVIM] Remove fzf-native extension to Telescope

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,12 +1,6 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
-    },
     opts = {
       defaults = {
         file_ignore_patterns = {

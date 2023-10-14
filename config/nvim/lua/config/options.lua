15e60fcd3664c718ce3f4a292706b0845eb97cd0
Alessio Rocco <alessiorocco@nebulab.com>
2023-10-14 12:39:33 +0200

    [NVIM] Let Lazy care about backups and swapfiles

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,8 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Autoread and autowrite
-vim.opt.autowriteall = true -- Like 'autowrite' but also used for commands
-vim.opt.swapfile = false -- Don't create a swapfile for a new buffer
-vim.opt.writebackup = false -- Don't backup before overwriting a file
 vim.opt.colorcolumn = "+1" -- Highlight the column next to the textwidth

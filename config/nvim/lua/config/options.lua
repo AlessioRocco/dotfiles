0274fb1abc6b7769478d0acaf1b3986016ee9321
Alessio Rocco <alessiorocco@nebulab.com>
2024-02-24 11:47:53 +0100

    [NVIM] Highlight the column next to the textwidth

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,4 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.colorcolumn = "+1" -- Highlight the column next to the textwidth
 

5a8c268c4a0e8e687897e24d6ab405963b43e20f
Alessio Rocco <alessiorocco@nebulab.com>
2024-03-08 08:58:38 +0100

    [NVIM] Add LazyVim Dot Files extra
    https://www.lazyvim.org/extras/util/dot

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.ruby" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.util.dot" },
     -- import/override with your plugins
     { import = "plugins" },
   },

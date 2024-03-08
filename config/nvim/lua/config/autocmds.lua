e10044daa1d1b673e4c0a2c033001f72d0e164f1
Alessio Rocco <alessiorocco@nebulab.com>
2024-03-08 18:07:50 +0100

    [NVIM] Use ctags in erb files

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Use ctags to jump to the definition in eruby files since Solargraph doesn't
+-- work in them.
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("ctags", { clear = true }),
+  pattern = { "eruby" },
+  callback = function()
+    vim.keymap.set("n", "gd", "<c-]>")
+  end,
+})

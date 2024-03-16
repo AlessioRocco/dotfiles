d7555011d60e61fc70c3ff504979e644dbaf73fb
Alessio Rocco <alessiorocco@nebulab.com>
2024-03-16 09:52:33 +0100

    [WEZ] Add fonts configuration
    https://www.youtube.com/watch?v=mQdB_kHyZn8&t=543s

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -9,5 +9,21 @@ local config = wezterm.config_builder()
 -- Changing the color scheme:
 config.color_scheme = "catppuccin-macchiato"
 
+-- Font settings:
+
+-- This video was helpful in understanding how to set up fonts:
+-- https://www.youtube.com/watch?v=mQdB_kHyZn8&t=543s
+config.font_size = 16
+config.font = wezterm.font_with_fallback({
+  "JetBrains Mono",
+  { family = "Symbols Nerd Font Mono", scale = 0.75 },
+})
+-- Use the cap-height font metrics of the base and the current font to adjust
+-- the size of secondary fonts (such as bold or italic faces) to visually match
+-- the size of the base font.
+-- For more information, see:
+-- https://wezfurlong.org/wezterm/config/lua/config/use_cap_height_to_scale_fallback_fonts.html
+config.use_cap_height_to_scale_fallback_fonts = true
+
 -- and finally, return the configuration to wezterm
 return config

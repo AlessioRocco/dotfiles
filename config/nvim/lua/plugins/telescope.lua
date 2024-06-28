1902bcafbdd629056252668de7e59bbbff2946d1
Alessio Rocco <alessiorocco@nebulab.com>
2024-06-28 18:08:56 +0200

    feat(nvim): ignore db/sample.sql in telescope search
    The file db/sample.sql has been added to the ignore patterns in
    telescope.lua. This change prevents the file from appearing in
    search results, improving search efficiency.

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
       defaults = {
         file_ignore_patterns = {
           "^spec/cassettes/",
+          "db/sample.sql",
         },
         path_display = { "truncate" },
       },

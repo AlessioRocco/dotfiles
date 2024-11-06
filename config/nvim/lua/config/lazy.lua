20aa23e92d0108dc16c1972bb0576ba5da97df35
Alessio Rocco <alessiorocco@nebulab.com>
2024-11-06 21:54:46 +0100

    feat(nvim): add markdown plugin configuration
    - Added lazyvim.plugins.extras.lang.markdown import in lazy.lua
    - Enhanced markdown support with new plugin configuration

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.util.dot" },
     { import = "lazyvim.plugins.extras.util.octo" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
     -- import/override with your plugins
     { import = "plugins" },
   },

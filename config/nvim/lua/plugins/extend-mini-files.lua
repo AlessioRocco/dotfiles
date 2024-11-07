2d3277319be8948a7fd0e8a5542174097e580398
Alessio Rocco <alessiorocco@nebulab.com>
2024-11-07 08:07:35 +0100

    refactor(nvim): extend mini.files plugin configuration
    - Renamed mini_files.lua to extend-mini-files.lua for clarity
    - Added new keymaps for opening mini.files in cwd and root directory
    - Set mini.files as the default file explorer
    - Configured preview window width to 100

---
+++ config/nvim/lua/plugins/extend-mini-files.lua
@@ -0,0 +1,34 @@
+return {
+  "echasnovski/mini.files",
+  keys = {
+    {
+      "-",
+      function()
+        require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+      end,
+      desc = "Open mini.files (directory of current file)",
+    },
+    {
+      "<leader>E",
+      function()
+        require("mini.files").open(vim.uv.cwd(), true)
+      end,
+      desc = "Open mini.files (cwd)",
+    },
+    {
+      "<leader>e",
+      function()
+        require("mini.files").open(LazyVim.root(), true)
+      end,
+      desc = "Open mini.files (root)",
+    },
+  },
+  opts = {
+    windows = {
+      width_preview = 100,
+    },
+    options = {
+      use_as_default_explorer = true,
+    },
+  },
+}

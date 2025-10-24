ecf86962b1e48038c81fd02fa7fd59a7b61af59b
Alessio Rocco <alessiorocco@nebulab.com>
2025-10-24 22:29:15 +0200

    Refactor mini.files keymaps for clarity and consistency
    Remove redundant <leader>e mapping and update <leader>E to "_" for
    opening mini.files in the current working directory.
    This streamlines navigation keybindings and reduces overlap, making file
    management more intuitive.

--- config/nvim-lazyvim/lua/plugins/extend-mini-files.lua
+++ config/nvim-lazyvim/lua/plugins/extend-mini-files.lua
@@ -9,18 +9,11 @@ return {
       desc = "Open mini.files (directory of current file)",
     },
     {
-      "<leader>E",
+      "_",
       function()
         require("mini.files").open(vim.uv.cwd(), true)
       end,
       desc = "Open mini.files (cwd)",
-    },
-    {
-      "<leader>e",
-      function()
-        require("mini.files").open(LazyVim.root(), true)
-      end,
-      desc = "Open mini.files (root)",
     },
   },
   opts = {
